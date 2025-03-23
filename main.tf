d9bcc5fd5138e785b96af4a5abb168e05fc580db
Paris Hoffman <47543650+parishoffman@users.noreply.github.com>
2025-03-23 09:46:33 -0700

    ec2

--- main.tf
+++ main.tf
@@ -2,3 +2,17 @@ module "vpc" {
   source    = "./vpc"
   namespace = var.namespace
 }
+
+module "ssh-key" {
+  source    = "./ssh-key"
+  namespace = var.namespace
+}
+
+module "ec2" {
+  source    = "./ec2"
+  namespace = var.namespace
+  vpc = module.vpc.vpc
+  sg_pub_id = module.vpc.sg_pub_id
+  sg_priv_id = module.vpc.sg_priv_id
+  key_name = module.ssh-key.key_name
+}
