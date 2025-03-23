586883673f7de57c13cd3e648005659f22d2c73e
Paris Hoffman <47543650+parishoffman@users.noreply.github.com>
2025-03-23 14:41:50 -0700

    fixed key

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,5 +1,3 @@
-
-
 // Create aws_ami filter to pick up the ami available in your region
 data "aws_ami" "amazon-linux-2" {
   most_recent = true
--- ssh-key/main.tf
+++ ssh-key/main.tf
@@ -1,14 +1,24 @@
+resource "random_string" "random" {
+  length = 4
+  special = false
+  upper = false
+}
+
+locals {
+  key_name = "${var.namespace}-${random_string.random.id}"
+}
+
 resource "tls_private_key" "key" {
   algorithm = "RSA"
 }
 
 resource "local_file" "private_key" {
-  filename          = "${var.namespace}-key.pem"
+  filename          = "${local.key_name}.pem"
   sensitive_content = tls_private_key.key.private_key_pem
   file_permission   = "0400"
 }
 
 resource "aws_key_pair" "key_pair" {
-  key_name   = "${var.namespace}-key"
+  key_name   = local.key_name
   public_key = tls_private_key.key.public_key_openssh
 }
